f9b5ea9c5567193cdd1dca79ee7e7d0e3550e460
KTaclysme <takevin.pro@gmail.com>
2024-06-20 15:57:54 +0200

    refacto(src/back): mf refacto

--- src/back/routes/routes.ts
+++ src/back/routes/routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import * as messageController from '../controllers/controllers';
+import express from 'express';
+import { sendMessageController, receiveMessageController } from '../controllers/controllers';
 
-const router = Router();
+const router = express.Router();
 
-router.post('/se', messageController.sendMessage);
-router.get('/re', messageController.receiveMessages);
+router.post('/send', sendMessageController);
+router.get('/receive', receiveMessageController);
 
 export default router;
